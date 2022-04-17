3bdef29b64a33a86f9df42dc39db5e8e1ba36b53
ArtemDragunov <artemdragunov123@gmail.com>
2022-04-17 13:47:40 +0300

    second commit

---
+++ index1.js
@@ -0,0 +1,13 @@
+let smt = 1*prompt();
+if (typeof smt === "string"){
+  alert(smt.length); 
+} else
+{
+  let smt2 = 1*prompt();
+if (typeof smt2 === "string")
+{alert('Enter number')}
+else{
+  let sum = 1*smt + 1*smt2
+  alert(sum);
+}
+}
